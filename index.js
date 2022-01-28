ee3a7614359d2d9afd43c8484a91588488087783
Daeyoung <daeyoungchoi@hotmail.com>
2022-01-27 18:53:57 -0800

    commit tree 84ba3954dd9a2bb052445c0d5d0065eae910f0d9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("play basketball"))
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+console.log(mondayWork("sleep all day long"))
+
+
+const wrapAdjective=function(flair="*") {
+    return function inner(adjective="special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
+
